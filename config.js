8534a8ec37b842aa121af7d972d29be7c8150d46
boomresttwo-art <boomrest.two@gmail.com>
2025-10-19 21:12:53 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // IMPORTANT: paste your Fiverr affiliate link below.
 // Example: export const AFFILIATE_URL = "https://www.fiverr.com/your-affiliate-link";
-export const AFFILIATE_URL = "https://www.fiverr.com/your-affiliate-link";
+export const AFFILIATE_URL = "https://www.fiverr.com/scraping_pal/do-b2b-lead-generation-and-build-a-prospect-list?context_referrer=tailored_homepage&source=views_related&ref_ctx_id=9ab2f5c7f850424e80c23d4f8db05d23&context=recommendation&pckg_id=1&pos=2&context_alg=gig_views_graph_v2&seller_online=true&imp_id=1e5f0d09-ccd9-4271-9b35-68f68bab0e70";
